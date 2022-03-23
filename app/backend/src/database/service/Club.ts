0535871f49643fe4eec3929da18f725a750e41d6
Wallacy Francis <wallacy_francis@hotmail.com>
2022-03-23 13:33:03 -0300

    Resolvido saida da rota /clubs

--- app/backend/src/database/service/Club.ts
+++ app/backend/src/database/service/Club.ts
@@ -1,9 +1,16 @@
 import ClubModel from '../models/Club'
 
+const parseCamelCase = (arr: object[]) => {
+  return arr.map((item: any) => ({
+    id: item.id,
+    clubName: item.club_name
+  }));
+}
+
 export default class Club {
   static async findAll() {
     const clubs = await ClubModel.findAll()
-    return clubs;
+    return parseCamelCase(clubs);
   }
 
   static async findByPk(id: number) {
